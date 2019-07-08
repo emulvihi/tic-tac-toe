e389853b3a190d194cedcd83e6d11963010c2cd4
Ubuntu <ubuntu@ip-172-31-87-212.ec2.internal>
2019-07-08 02:58:08 +0000

    Add game functionality; add unbeatable AI using minimax algorithm

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -89,7 +89,7 @@
 
 
 // Make array of elements
-var tiles = document.getElementByClassName("tile");
+var tiles = document.getElementsByClassName("tile");
 var buttons = document.getElementsByClassName("button");
 
 // Sets initial state of game and board
@@ -103,6 +103,18 @@ var COMPUTER = true;
 // Sets values for computer and player moves
 var HUMVAL = -1;
 var COMVAL = 1;
+
+// List of possible win conditions
+var winMatrix = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+    ];
 
 // Resets board to initial state
 function reset() {
@@ -121,5 +133,113 @@ function reset() {
 }
 
 function claim(clicked) {
+    // Prevents this function from running if there is no game
+    if(!game)
+        return;
+        
+    for (var x = 0; x < 9; x++) {
+        // Checks to see if tile is unoccupied and fills it if so
+        if (tiles[x] == clicked && state[x] == 0) {
+            set(x, HUMAN);
+            callAI();
+        }
+    }
+}
+
+function set(index, player) {
+    // Prevents this function from running if there is no game
+    if(!game)
+        return;
     
+    // Hides Computer button when game is being played
+    if (state[index] == 0) {
+        buttons[0].style.width = "0";
+        buttons[0].style.margin = "0";
+        buttons[0].style.opacity = "0";
+        
+        buttons[1]. style.width = "32vh";
+        
+    // Sets background of tile based on whether it is taken by the player or the computer
+        if (player == HUMAN) {
+            tiles[index].style.background = "#cc0c0c";
+            state[index] = HUMVAL;
+        } else {
+            tiles[index].style.background = "#0c39cc";
+            state[index] = COMPUTER;
+        }
+        
+    // Ends game if win state has been reached
+        if (checkWin(state, player)) {
+            game = false;
+        }
+    }
+}
+
+// Checks board to see if win conditions have been fulfilled
+function checkWin(board, player) {
+    // If the player is human, set value to the human value; otherwise, set it to the computer value
+    var value = player == HUMAN ? HUMVAL : COMVAL;
+    
+    for (var x = 0; x < 8; x++) {
+        // Win state is set to a default of true
+        var win = true;
+        
+        // Sets win state to false if no win condition is met
+        for (var y = 0; y < 3; y++) {
+            if (board[winMatrix[x][y]] != value) {
+                win = false;
+                break;
+            }
+        }
+        
+        if (win)
+            return true;
+    }
+    
+    return false;
+}
+
+// Checks for draw state
+function checkFull(board) {
+    for (var x = 0; x < 9; x++) {
+        if (board[x] == 0)
+            return false;
+    }
+    return true;
+}
+
+function callAI() {
+    aiturn(state, 0, COMPUTER);
+}
+
+function aiturn(board, depth, player) {
+    if (checkWin(board, !player))
+        return -10 + depth;
+        
+    if (checkFull(board))
+        return 0;
+        
+    var value = player == HUMAN ? HUMVAL : COMVAL;
+    
+    var max = -Infinity;
+    var index = 0;
+        
+    for (var x = 0; x < 9; x++) {
+        if (board[x] == 0) {
+            var newboard = board.slice();
+            newboard[x] = value;
+            
+            var moveval = -aiturn(newboard, depth + 1, !player);
+            
+            if (moveval > max) {
+                max = moveval;
+                index = x;
+            }
+        }
+    }
+    
+    if (depth == 0) 
+        set(index, COMPUTER);
+    
+    return max;
 }
